fda1bb0487edf281610caf9545b37a276ea69cc0
Binben Wang <binbenwang1994@gmail.com>
2019-08-18 12:53:31 -0600

    Connected to the database

--- server.js
+++ server.js
@@ -2,86 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'bwang',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+
+// db.select('*').from('users').then(data => {console.log(data)});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 7,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 });
 
-app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
-});
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 
-app.post('/register', (req, res) => {
-    const { email, name, password} = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length - 1]);
-});
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach( user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    });
-    if (!found) {
-        res.status(404).json('no such user');
-    }
-});
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach( user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(404).json('no such');
-    }
-});
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
