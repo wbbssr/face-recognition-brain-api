a1c59d4826bcd5600fb85624bfd70e453a11a9c7
Binben Wang <binbenwang1994@gmail.com>
2019-08-18 15:03:11 -0600

    Removed fake database array

--- server.js
+++ server.js
@@ -19,15 +19,13 @@ const db = knex({
     }
 });
 
-// db.select('*').from('users').then(data => {console.log(data)});
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send("It is working!");
 });
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
@@ -39,6 +37,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log("app is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
